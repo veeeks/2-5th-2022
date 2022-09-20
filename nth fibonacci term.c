a66a52443f8b775ef11ea18458a8f5809ccf3c0c
veeeks <43359785+veeeks@users.noreply.github.com>
2022-09-20 18:07:29 +0530

    Day 6 - Fibonacci series in C
    Program in C to find the nth Fibonacci  term.

---
+++ nth fibonacci term.c
@@ -0,0 +1,27 @@
+//cerner_2tothe5th_2022
+#include <stdio.h>
+unsigned long long fibo(int num);
+int main()
+{
+    int num;
+    unsigned long long fibonacci;
+    
+    /* Input a number from user */
+    printf("Enter any number to find nth fiboacci term: ");
+    scanf("%d", &num);
+    
+    fibonacci = fibo(num); 
+    
+    printf("%d fibonacci term is %llu", num, fibonacci);
+    
+    return 0;
+}
+unsigned long long fibo(int num) 
+{
+    if(num == 0) 
+        return 0;
+    else if(num == 1)
+        return 1;
+    else 
+        return fibo(num-1) + fibo(num-2); 
+}
