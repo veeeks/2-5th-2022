df9f18146ef511e71c014ea5f6e50adbd2233709
veeeks <43359785+veeeks@users.noreply.github.com>
2022-10-12 20:27:32 +0530

    Day 28 - DLang
    Program in Dlang to return the sum of two digits.

---
+++ Sum.d
@@ -0,0 +1,14 @@
+////cerner_2tothe5th_2022
+import std.stdio, std.conv;
+
+void main()
+{
+  
+    int number1, number2;
+    writef("Enter  a number: ");
+    readf(" %s", &number1);
+    writef("Enter  another number: ");
+    readf(" %s", &number2);
+    writef( "Sum is " );
+    writeln(number1+ number2);
+}
